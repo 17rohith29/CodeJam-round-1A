f104f7d8e5292745fed840446c680cd96b092fcb
Rohith Prabakar <17rohith29@gmail.com>
2018-04-13 23:50:04 -0400

    Update C.py

--- C.py
+++ C.py
@@ -5,23 +5,18 @@ def cut(a, b):
 	val[0] = 2*a
 	val[1] = 2*b
 	val[2] = ((a**2 + b**2)**0.5 + a + b)*2-(a+b)*2
-	val.sort()
-	if val[1]==val[0]:
-		val[1] = val[2]
-	val[2] = val[2]-val[1]
-	val[1] = val[1] - val[0]
-	return val
+	return [min(val), max(val)]
 
 def mor(val, dif):
 	l = dif 
-	for i in range(0,3):
-		for j in val:
-			y = j[i]
-			if (l-y < 0):
-				if i == 2:
-					return 0
-			else:
-				l -= y
+	for j in val:
+		a,b = j[0],j[1]
+		if (l-a) < 0:
+			return l
+		elif (l-b) < 0:
+			return 0
+		else:
+			l -= b
 	return l
 
 
@@ -33,7 +28,6 @@ for i in range(0, int(input())):
 		w, h = map(int, input().split())
 		curPer += 2*(w+h)
 		val.append(cut(w, h))
-	val = sorted(val, key=itemgetter(0,1,2))
 	dif = p-curPer
 	out = None
 	if dif <= 0:
